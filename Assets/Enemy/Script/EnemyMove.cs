4a832f1468f9943b4a91db19187d1d61401a6630
ths70679_Yamashita <faire245@gmail.com>
2019-05-07 11:33:53 +0900

    敵の徘徊を追加

--- Assets/Enemy/Script/EnemyMove.cs
+++ Assets/Enemy/Script/EnemyMove.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
+
 
 public class EnemyMove : MonoBehaviour
 {
@@ -11,17 +13,28 @@ public class EnemyMove : MonoBehaviour
     public bool IsAttack;
     public int HP;
     private EnemyStatus enemystatus;
+    // ------------------------------------------------------
+    NavMeshAgent agent;                             //徘徊用
+    [SerializeField] private int destPoint = 0;     // 巡回地点のオブジェクト数（初期値=0）
+    [SerializeField] private Transform[] points;    //巡回する地点のオブジェクト
+    
 
-    // Start is called before the first frame update
+    
     void Start()
     {
         Player = GameObject.Find("Player");
         enemystatus = GetComponent<EnemyStatus>();
         IsAttack = false;
         HP = 10;
+
+        //経路探索（巡回）
+        agent = GetComponent<NavMeshAgent>();           //経路のアタッチ
+        agent.autoBraking = false;                      //減速回避
+        GotoNextPoint();                                //初期の巡回ポイントを確保
     }
 
-    // Update is called once per frame
+
+
     void Update()
     {
         PlayerDes = Player.transform.position;
@@ -31,10 +44,18 @@ public class EnemyMove : MonoBehaviour
             if (IsAttack==true)
             {
                 //プレイヤー座標の方に少しずつ向きが変わる
-                transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(PlayerDes - transform.position), 0.1f);
+                //transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(PlayerDes - transform.position), 0.1f);
 
                 //プレイヤーの居場所に向かって進む
-                transform.position += transform.forward * Speed;
+                //transform.position += transform.forward * Speed;
+                agent.SetDestination(PlayerDes);
+            }
+            else
+            {
+                // 巡回ポイントの通貨を確認
+                if (!agent.pathPending && agent.remainingDistance < 0.5f)
+                    // 次ポイント取得
+                    GotoNextPoint();
             }
         }
         enemystatus.SetHp(HP);
@@ -61,5 +82,18 @@ public class EnemyMove : MonoBehaviour
     public void Dead()
     {
         Destroy(this.gameObject);
+    }
+
+    // 次の巡回地点を設定する処理
+    private void GotoNextPoint()
+    {
+        // 巡回地点が設定されていなければ
+        if (points.Length == 0)
+            return;
+
+        // 現在選択されている配列の座標を巡回地点の座標に代入
+        agent.destination = points[destPoint].position;
+        // 配列の中から次の巡回地点を選択（必要に応じて繰り返し）
+        destPoint = (destPoint + 1) % points.Length;
     }
 }
