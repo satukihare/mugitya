70f7030e3fee954d38e51efb74ee8affebdda230
ths70679_Yamashita <faire245@gmail.com>
2019-05-15 09:51:14 +0900

    アリの挙動

---
+++ Assets/Enemy/HitSearchErea.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitSearchErea : MonoBehaviour
+{
+    private bool b_ereaIn;
+
+    void Start()
+    {
+        b_ereaIn = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    // 情報を親へ渡すためのGetter
+    public bool getEreaEnable()
+    {
+        return b_ereaIn;
+    }
+    
+    //　エリアに侵入
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Debug.Log("end");
+            b_ereaIn = true;
+        }
+    }
+    //エリアから脱出
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Debug.Log("end");
+            b_ereaIn = false;
+        }
+    }
+}
