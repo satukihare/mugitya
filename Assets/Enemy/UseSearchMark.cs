3154c07c44303bb5aa102f1f030cecddbfe92c04
nobunari <gintaa388@gmail.com>
2019-05-22 17:41:17 +0900

    虫びっくりマーク実装

---
+++ Assets/Enemy/UseSearchMark.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UseSearchMark : MonoBehaviour
+{
+    private Transform Mark;
+    bool IsUseMark;
+    int Timer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Timer = 0;
+        IsUseMark = false;
+        Mark= this.transform.Find("SearchMark");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(IsUseMark==true)
+        {
+            Timer++;
+        }
+        if(Timer>=120)
+        {
+            Timer = 0;
+            IsUseMark = false;
+            Mark.gameObject.SetActive(false);
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag=="fire" && IsUseMark==false)
+        {
+            IsUseMark = true;
+            Mark.gameObject.SetActive(true);
+        }
+    }
+}
