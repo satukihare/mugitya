3110599ef43fc90a6c64fcbee152f13015b0a181
五月晴れ <gundam.zakizaki@gmail.com>
2019-05-23 12:27:42 +0900

    fireWallを修正した

--- Assets/fireWall/script/ControlFireWall.cs
+++ Assets/fireWall/script/ControlFireWall.cs
@@ -11,6 +11,8 @@ public class ControlFireWall : MonoBehaviour {
     //壁が出現する高さ
     public float wall_height = 3.0f;
 
+    public float wall_z_move = 10.0f;
+
     private List<GameObject> fire_obj_list = new List<GameObject>();
 
     // Start is called before the first frame update
@@ -19,7 +21,9 @@ public class ControlFireWall : MonoBehaviour {
 
             //Instantiate( 生成するオブジェクト,  場所, 回転 );  回転はそのままなら↓
             //Instantiate((GameObject)Resources.Load("fireWallObj"), new Vector3(0 + count / 10, 0, 0), Quaternion.identity);
-            GameObject temp = Instantiate((GameObject)Resources.Load("fireWallObj"), new Vector3(count + camera.transform.position.x - (wall_nums / 2), wall_height, camera.transform.position.z + 8.0f), Quaternion.identity);
+            GameObject temp = Instantiate((GameObject)Resources.Load("fireWallObj"),
+                new Vector3(count + camera.transform.position.x - (wall_nums / 2), wall_height, camera.transform.position.z),
+                Quaternion.identity);
             fire_obj_list.Add(temp);
         }
     }
@@ -30,11 +34,12 @@ public class ControlFireWall : MonoBehaviour {
 
         //Vector3 temp = fire_obj.transform.position;
         //fire_obj.transform.position = new Vector3(temp.x, temp.y, temp.z+0.02f);
-
-        //スペースを押したら
-        if (Input.GetKeyDown(KeyCode.Space)) {
-            //Instantiate( 生成するオブジェクト,  場所, 回転 );  回転はそのままなら↓
-            Instantiate((GameObject)Resources.Load("fireWallObj"), new Vector3(1.0f, 2.0f, 0.0f), Quaternion.identity);
+        
+        for(int count = 0; count < fire_obj_list.Count; count++) {
+            GameObject temp = fire_obj_list[count];
+            
+            temp.transform.position = new Vector3(temp.transform.position.x, temp.transform.position.y, camera.transform.position.z+ wall_z_move);
         }
+
     }
 }
