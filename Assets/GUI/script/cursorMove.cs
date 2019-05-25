f300cbe7eccc759e2c64f4798e922d1c885a3dfb
五月晴れ <gundam.zakizaki@gmail.com>
2019-05-26 00:55:01 +0900

    pause画面の画面レイアウトの構築 pause画面時のメニューの動作の完成

---
+++ Assets/GUI/script/cursorMove.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cursorMove : MonoBehaviour {
+    public GamePad game_pad = null;
+
+    public List<GameObject> img_obj = new List<GameObject>(2);
+
+    //cursorの位置
+    private int cursor_position_num = 0;
+
+    //メニュー最大数
+    private const int min_menu_num = 0;
+    //メニュー最小数
+    private const int max_menu_num = 1;
+
+    //ゲームパッド移動フラグ
+    private bool pad_move_flg = false;
+
+    //cursor移動量
+    public float cursor_move_num = 0.2f;
+
+    // Start is called before the first frame update
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        checkCursorMove();
+
+        cursorMovePosition();
+    }
+
+    //Cursorの移動判定
+    private void checkCursorMove() {
+
+        float pad_move_x_num = game_pad.GetLeftStickX();
+        float pad_move_y_num = game_pad.GetLeftStickY();
+
+        pad_move_flg = pad_move_x_num == 0 && pad_move_y_num == 0 ? false : true;
+
+        //↑うえ
+        if (Input.GetKeyDown(KeyCode.W) || pad_move_y_num >= 1.0f && pad_move_flg == false) {
+            cursor_position_num--;
+            if (cursor_position_num < min_menu_num)
+                cursor_position_num = min_menu_num;
+
+            pad_move_flg = true;
+        }
+
+        //↓した
+        if (Input.GetKeyDown(KeyCode.S) || pad_move_y_num <= -1.0f && pad_move_flg == false) {
+            cursor_position_num++;
+            if (cursor_position_num > max_menu_num)
+                cursor_position_num = max_menu_num;
+            pad_move_flg = true;
+        }
+
+    }
+
+    //Cursorの移動
+    private void cursorMovePosition() {
+        //今いるところ
+        Vector2 now_position = transform.position;
+        //目的のUI
+        Vector2 go_to_position = img_obj[cursor_position_num].transform.position;
+        //移動量
+        Vector2 movement = new Vector2(0.0f, 0.0f);
+
+        //上にあったら下へ
+        if (now_position.y > go_to_position.y) {
+            movement.y -= cursor_move_num;
+            if (movement.y + now_position.y < go_to_position.y) {
+                movement.y = 0.0f;
+                transform.position = new Vector2(now_position.x, go_to_position.y);
+            }
+            else
+                transform.position = new Vector2(now_position.x, movement.y + transform.position.y);
+
+            return;
+        }
+
+        //下にあったら上へ
+        if (now_position.y < go_to_position.y) {
+            movement.y += cursor_move_num;
+            if (movement.y + now_position.y > go_to_position.y) {
+                movement.y = 0.0f;
+                transform.position = new Vector2(now_position.x, go_to_position.y);
+            }
+            else
+                transform.position = new Vector2(now_position.x, movement.y + transform.position.y);
+            return;
+        }
+    }
+
+    //Cursorの位置を渡す
+    public int getCursorPosision() {
+        return cursor_position_num;
+    }
+}
---
+++ Assets/GUI/script/pauseControl.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pauseControl : MonoBehaviour {
+    //パッド
+    public GamePad game_pad = null;
+    //pause画面
+    public GameObject pouse_ui = null;
+
+    //pauseのフラグ
+    private bool pause_mode_flg = false;
+
+    public cursorMove cursor = null;
+
+    // Start is called before the first frame update
+    void Start() {
+    }
+
+    // Update is called once per frame
+    void Update() {
+        //pauseButtonが押されたか
+        pauseButtonCheck();
+
+        //pause中にpauseメニューの動作判定
+        if (pause_mode_flg)
+            pauseActionCheck();
+
+        //pauseなら停止させる
+        if (pause_mode_flg) {
+            Time.timeScale = 0.0f;
+            pouse_ui.SetActive(true);
+        }
+        else if (!pause_mode_flg) {
+            Time.timeScale = 1.0f;
+            pouse_ui.SetActive(false);
+        }
+    }
+
+    //pauseの判定
+    private void pauseButtonCheck() {
+
+        //ポーズボタン判定が押されたたら止まる
+        if (game_pad.GetMenuButton() || Input.GetKeyDown(KeyCode.Escape)) {
+            pause_mode_flg = pause_mode_flg ? false : true;
+        }
+    }
+
+    private void pauseActionCheck() {
+
+        //決定Buttonが押されたら
+        if (Input.GetKeyDown(KeyCode.Return) || game_pad.GetCircle()) {
+            //cursorの位置
+            int cursor_pos_num = cursor.getCursorPosision();
+
+            if (0 == cursor_pos_num) {
+                pause_mode_flg = false;
+            }
+            if (1 == cursor_pos_num) {
+                Time.timeScale = 1f;
+                FadeManager.FadeOut(0);
+                pause_mode_flg = false;
+            }
+        }
+    }
+}
